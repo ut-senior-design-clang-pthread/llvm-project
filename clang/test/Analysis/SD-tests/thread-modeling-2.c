2aad7d093f89dc678c1c3b4169cef7901c5fd816
Victoria Nguyen <victorianguyen@wireless-10-145-5-214.public.utexas.edu>
2025-02-03 16:15:43 -0600

    removed include flag and enumerated bool

--- clang/test/Analysis/SD-tests/thread-modeling-2.c
+++ clang/test/Analysis/SD-tests/thread-modeling-2.c
@@ -1,30 +1,26 @@
 // RUN: %clang_analyze_cc1 -Wno-strict-prototypes -Wno-error=implicit-int -verify  %s \
-// RUN: -I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include \
 // RUN:   -analyzer-checker=debug.ExprInspection
 
 #define NULL ((void*) 0)
-#include <stdbool.h>
+enum bool {
+	false,
+	true
+};
+
 
 typedef unsigned long int pthread_t;
 typedef struct __pthread_attr pthread_attr_t;
 
-int pthread_create(pthread_t *restrict thread,
-
-       const pthread_attr_t *restrict attr,
-
-       void *(*start_routine)(void*), void *restrict arg);
-
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
 
 
 int pthread_join(pthread_t thread, void **retval);
 
-
-
-void clang_analyzer_checkInlined(bool);
+void clang_analyzer_checkInlined(enum bool);
 
 void* thread_function(void* arg){
 	// should expect to fail the test at this line if you set the checkInlined to true
-	clang_analyzer_checkInlined(false);		// no-warning
+	clang_analyzer_checkInlined(true);		// expected-warning{{TRUE}}
 	return NULL;
 }
 
