6d0d0b3df6d883b8ad0c89a9b6b4986a092bfa2b
Victoria Nguyen <victorianguyen@wireless-10-147-78-120.public.utexas.edu>
2025-02-07 09:57:00 -0600

    adding warnIfReached

--- clang/test/Analysis/SD-tests/thread-modeling-2.c
+++ clang/test/Analysis/SD-tests/thread-modeling-2.c
@@ -1,5 +1,8 @@
 // RUN: %clang_analyze_cc1 -Wno-strict-prototypes -Wno-error=implicit-int -verify  %s \
-// RUN:   -analyzer-checker=debug.ExprInspection
+// RUN:   -analyzer-checker=debug.ExprInspection \
+// RUN:   -analyzer-checker=core \
+// RUN:   -analyzer-checker=unix
+
 
 #define NULL ((void*) 0)
 enum bool {
@@ -17,10 +20,17 @@ int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_
 int pthread_join(pthread_t thread, void **retval);
 
 void clang_analyzer_checkInlined(enum bool);
+void clang_analyzer_warnIfReached();
 
 void* thread_function(void* arg){
 	// should expect to fail the test at this line if you set the checkInlined to true
 	clang_analyzer_checkInlined(true);		// expected-warning{{TRUE}}
+    if(true){
+      clang_analyzer_warnIfReached();      //expected-warning{{REACHABLE}}
+    }
+    else{
+      clang_analyzer_warnIfReached();        // no-warning
+    }
 	return NULL;
 }
 
@@ -28,6 +38,12 @@ int foo_no_pthread(void)
 {
 	// should expect to pass at this line
 	clang_analyzer_checkInlined(true);		// expected-warning{{TRUE}}
+  if(true){
+    clang_analyzer_warnIfReached();      //expected-warning{{REACHABLE}}
+  }
+  else{
+    clang_analyzer_warnIfReached();        // no-warning
+  }
 	return 0;
 }
 
